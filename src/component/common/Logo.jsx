7057f5034e291fe604ef3080a2e0333e33b118ea
Yass <YassineSalhi135@gmail.com>
2021-06-01 16:17:19 +0100

    portofolio

---
+++ src/component/common/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+
+export const Logo = () => (
+    <div className="logo">
+        <Link to="/">
+            {/* <img src="/assets/images/logo/logo-black.png" alt="Digital Agency"/> */}
+            <h2>YS</h2>
+        </Link>
+    </div>
+)
+
